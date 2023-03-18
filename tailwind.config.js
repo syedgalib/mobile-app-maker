3ada28ef379a9d9b4eb999e743e3a92a3a7b4a48
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-03-18 12:08:14 +0600

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/ts/*.tsx',
+    './src/ts/*.jsx',
+    './view/*.php',
+    './template/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
